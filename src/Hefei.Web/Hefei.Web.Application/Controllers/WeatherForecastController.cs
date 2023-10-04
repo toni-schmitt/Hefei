2ddc194703bc4126457fcbf179a1cf520b98051a
Toni Schmitt <tonischmitt@online.de>
2023-10-04 22:30:43 +0200

    chore: add inital recat webapp

---
+++ src/Hefei.Web/Hefei.Web.Application/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hefei.Web.Application.Controllers;
+
+[ApiController]
+[Route(
+    "[controller]"
+)]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] s_summaries =
+    {
+        "Freezing",
+        "Bracing",
+        "Chilly",
+        "Cool",
+        "Mild",
+        "Warm",
+        "Balmy",
+        "Hot",
+        "Sweltering",
+        "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        => _logger = logger;
+
+    [HttpGet]
+    public IEnumerable<WeatherForecast> Get() => Enumerable.Range(
+            1,
+            5
+        )
+        .Select(
+            index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(
+                    DateTime.Now.AddDays(
+                        index
+                    )
+                ),
+                TemperatureC = Random.Shared.Next(
+                    -20,
+                    55
+                ),
+                Summary = s_summaries[Random.Shared.Next(
+                    s_summaries.Length
+                )]
+            }
+        )
+        .ToArray();
+}
---
+++ src/Hefei.Web/Hefei.Web.Application/Program.cs
@@ -0,0 +1,31 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(
+    args
+);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    "default",
+    "{controller}/{action=Index}/{id?}"
+);
+
+app.MapFallbackToFile(
+    "index.html"
+);
+
+app.Run();
